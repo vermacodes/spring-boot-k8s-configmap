cc5740912acf73c8cb10406120e0223781fa7978
Ashish Verma <ashishverma.pu@gmail.com>
2022-08-17 11:05:29 -0400

    Logging

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
     @Value("${welcome.message}")
     private String message;
 
     @GetMapping("/welcome")
     public String welcome(){
+        logger.info("Returning Message.");
         return message;
     }
 }
